ebce211b0cc284986a6ed73fc8cdcceddfef6a44
Gaurav <gauravsingla677@gmail.com>
2022-01-30 19:43:10 +0530

    winner function added

--- voting.sol
+++ voting.sol
@@ -2,23 +2,44 @@
 pragma solidity >=0.5.0 <0.8.0;
 
 contract voting{
+    uint public winner;
+    uint id;
+    uint[] u;
+    constructor() public {
+        id = 1;
+    }
     struct partcipant {
         address a;
         uint votes;
         bool added;
-    }    
-    mapping(address => partcipant) vid;
-    address[] a;
+    }
+    mapping(uint => partcipant) vid;
+    
 
     function addpartcipant(address _a) public returns(bool) {
-        a.push(_a);
-        vid[_a].votes = 0;
-        vid[_a].added = true;
-        return vid[_a].added;
+        require(vid[id].added == false);
+        u.push(id);
+        vid[id].a = _a;
+        vid[id].votes = 0;
+        vid[id].added = true;
+        id++;
+        return true;
     }
     function vote(uint votingid) public returns(address){
-        address add = a[votingid];
+        uint add = u[votingid];
         vid[add].votes++;
-        return add;
+        return vid[add].a;
+    }
+    function winnerd() public
+    {
+        uint v = 0;
+        for (uint i = 0; i < u.length; i++) 
+        {
+            if (vid[u[i]].votes > v)
+             {
+                v = vid[u[i]].votes;
+                winner = i;
+            }
+        }
     }
 }
