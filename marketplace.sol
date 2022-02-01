40b83b341a49a4ab2036eeb0559c95f899d2aaa9
Gaurav <gauravsingla677@gmail.com>
2022-02-01 23:31:27 +0530

    made few changes to buy function

--- marketplace.sol
+++ marketplace.sol
@@ -4,12 +4,7 @@ pragma solidity >=0.5.0 <0.8.0;
 contract marketplace {
 
     address[] public sellers;
-    modifier onlyowner() 
-    {
-        require(msg.sender == sellers[0] );
-        _;
-    }
-
+    
     struct item 
     {
         string name;
@@ -25,14 +20,14 @@ contract marketplace {
         itemno =1;
     }
 
-    function addsellers(address _s) public onlyowner() 
+    function addsellers(address _s) public 
     {
         sellers.push(_s);
     }
 
     mapping(uint => item) selling;
     mapping(address => uint[]) itemsbought;
-    uint[] itemb;
+    uint[] public itemb;
     
     function listitem(string memory _name,uint _id, uint _cost) public 
     {
@@ -51,23 +46,20 @@ contract marketplace {
 
     function buyitem(uint _id) public payable 
     {
-        address payable a;
-        for(uint i=0; i<itemno; i++)
+        for(uint i=1; i<itemno; i++)
         {
             if(selling[i].id == _id )
             {
                 require(msg.value == selling[i].cost);
-                a = payable(sellers[i]);
-                a.transfer(msg.value);
                 itemsbought[msg.sender].push(selling[i].id);
             }
         }
     }
-    function myorders() view public
+    /*function myorders()  view public
     { 
-        for(uint i=0; i<itemsbought[msg.sender].length; i++)
+        for(uint i=1; i<itemsbought[msg.sender].length; i++)
         {
         itemb[i];
         }
-    }
+    }*/
 }
